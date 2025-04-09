312cc408d653fdd4e82d0f96cb7fb19c5c9bbc1d
Дмитрий Шундрик <dmitryshundrik@gmail.com>
2025-04-09 18:29:01 +0300

    Add Swagger documentation, add dev test and prod properties, other minor changes

--- src/main/java/com/example/transactionservice/controller/ClientController.java
+++ src/main/java/com/example/transactionservice/controller/ClientController.java
@@ -5,6 +5,12 @@ import com.example.transactionservice.model.dto.LimitResponseDto;
 import com.example.transactionservice.model.dto.TransactionResponseDto;
 import com.example.transactionservice.service.LimitService;
 import com.example.transactionservice.service.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,23 +26,71 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/client")
 @RequiredArgsConstructor
+@Tag(name = "Client API", description = "API for managing client transactions and limits")
 public class ClientController {
 
     private final TransactionService transactionService;
 
     private final LimitService limitService;
 
+    @Operation(
+            summary = "Get list of exceeded transactions",
+            description = "Returns a list of all transactions that exceed the set limits."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "List retrieved successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = TransactionResponseDto.class)
+                    )
+            )
+    })
     @GetMapping("/exceeded-transactions")
     public ResponseEntity<List<TransactionResponseDto>> getExceededTransactions() {
         List<TransactionResponseDto> exceededTransactions = transactionService.getExceededTransactions();
         return ResponseEntity.ok(exceededTransactions);
     }
 
+    @Operation(
+            summary = "Get list of limits",
+            description = "Returns a list of all set limits."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "List of limits retrieved successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = LimitResponseDto.class)
+                    )
+            )
+    })
     @GetMapping("/limits")
     public List<LimitResponseDto> getLimits() {
         return limitService.getLimits();
     }
 
+    @Operation(
+            summary = "Create a new limit",
+            description = "Creates a new limit based on the provided data."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Limit created successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = LimitResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid request data",
+                    content = @Content
+            )
+    })
     @PostMapping("/limits")
     @ResponseStatus(HttpStatus.CREATED)
     public LimitResponseDto createLimit(@Valid @RequestBody LimitRequestDto limitRequestDto) {
--- src/main/java/com/example/transactionservice/controller/TransactionController.java
+++ src/main/java/com/example/transactionservice/controller/TransactionController.java
@@ -2,6 +2,11 @@ package com.example.transactionservice.controller;
 
 import com.example.transactionservice.model.dto.TransactionRequestDto;
 import com.example.transactionservice.service.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,10 +19,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Tag(name = "Transaction API", description = "API for managing transactions")
 public class TransactionController {
 
     private final TransactionService transactionService;
 
+    @Operation(
+            summary = "Create a new transaction",
+            description = "Creates a new transaction based on the provided data."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Transaction created successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid request data",
+                    content = @Content
+            )
+    })
     @PostMapping("/transactions")
     @ResponseStatus(HttpStatus.CREATED)
     public void createTransaction(@Valid @RequestBody TransactionRequestDto transactionRequestDto) {
